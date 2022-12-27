3973482f9310e365473f578c83f7fcb64e541b1e
Divyanshu Gangwar <dvyanshu@iitk.ac.in>
2022-12-27 15:47:15 +0530

    Add necessary React Components

---
+++ components/Contact.jsx
@@ -0,0 +1,94 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
+import ContactImg from '../public/assets/me2.jpg';
+
+const Contact = () => {
+
+  const scrollToTop = (e) => {
+    document.body.scrollIntoView({behavior: "smooth",});
+  }
+
+  return (
+    <div id='contact' className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
+        <p className='text-xl tracking-widest font-semibold uppercase text-[#5651e5]'>
+          Contact
+        </p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-2 gap-8 justify-items-center items-center'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full flex justify-center items-center lg:flex-col'>
+              <div className='relative w-2/5'>
+                <Image
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src={ContactImg}
+                  alt='divyanshu-gangwar'
+                />
+              </div>
+              <div>
+                <h2 className='py-8 text-center'>Divyanshu Gangwar</h2>
+              </div>
+              <div>
+                <p className='uppercase pt-8 text-center'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4 space-x-2'>
+                  <a
+                    href='https://www.linkedin.com/in/divyanshugangwar/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+
+                  <a
+                    href='https://github.com/Divyanshu23'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <a href="mailto:gangwardivyanshu23@gmail.com">
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <AiOutlineMail />
+                    </div>
+                  </a>
+
+                  <Link href='/resume'>
+                    <a>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill />
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className='flex justify-center py-12'>
+          <a onClick={scrollToTop}>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <HiOutlineChevronDoubleUp
+                className='text-[#5651e5]'
+                size={30}
+              />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ components/Projects.jsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import NextFashion from "../public/assets/projects/nextfashion.png"
+import NoteIt from "../public/assets/projects/noteit.png"
+import News from "../public/assets/projects/news_app.png"
+import TextUtility from "../public/assets/projects/textutility.png"
+
+const Projects = () => {
+  return (
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1280px] mx-auto py-16'>
+        <p className='text-xl tracking-widest font-semibold uppercase text-[#5651e5] mb-6'>
+          Work Experience & Projects
+        </p>
+        
+        <div className='flex justify-center items-center mb-8'>
+          <div className='relative flex items-center justify-center h-[36rem] w-full md:w-[40%] shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+            <Image className='rounded-xl group-hover:opacity-10' src="/assets/projects/me_amazon.jpg" alt='Divyanshu_Amazon' layout='fill' sizes="100vw" style={{ objectFit: 'contain' }} />
+            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+              <h3 className='text-2xl text-white tracking-wider text-center w-full'>Amazon SDE Intern</h3>
+              <p className='pb-4 pt-2 text-white text-center'>JAVA</p>
+              <Link href='/'>
+                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='NextFashion'
+            backgroundImg={NextFashion}
+            projectUrl='/nextfashion'
+            tech='Next.js'
+
+          />
+          <ProjectItem
+            title='NoteIt Cloud Notebook'
+            backgroundImg={NoteIt}
+            projectUrl='/noteit'
+            tech='React.js'
+
+          />
+          <ProjectItem
+            title='News Frontend App'
+            backgroundImg={News}
+            projectUrl='/news-frontend-app'
+            tech='React.js'
+
+          />
+
+          <ProjectItem
+            title='Text Utility App'
+            backgroundImg={TextUtility}
+            projectUrl='/textutility'
+            tech='React.js'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
